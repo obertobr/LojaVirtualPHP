efeb712867a260318c216b5284a02d9072f2369d
obertobr <real.azzi@gmail.com>
2023-06-13 21:36:47 -0300

    code for database connection

---
+++ site/functions/bd.php
@@ -0,0 +1,19 @@
+<?php
+
+function connection(){
+    $dsn = "mysql:host=localhost;dbname=loja";
+    $username = "root";
+    $password = "";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,
+                        PDO::ERRMODE_EXCEPTION);
+    return $conn;
+    
+}
+
+function verify() {
+    if (!isset($_SESSION['cpf-cnpj'])) {
+        header('Location: ../login.php');
+        exit;
+    }
+}
