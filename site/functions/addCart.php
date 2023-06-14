328a60eb5749ef23d5e6b9bd7052885e8aca2f06
obertobr <real.azzi@gmail.com>
2023-06-13 21:35:15 -0300

    adding items to cart

---
+++ site/functions/addCart.php
@@ -0,0 +1,28 @@
+<?php
+include_once './bd.php';
+
+verify()
+
+$codigo = $_GET["codigo"];
+
+$bd = connection();
+
+$sql = "SELECT * FROM carrinho WHERE cpf_cnpj_cli = '".$_SESSION['nome']."' AND codigo_prod = '$codigo'";
+$result = $bd->query($sql);
+
+if ($result->rowCount() > 0) {
+    $row = $result -> fetch();
+    $sql = "UPDATE carrinho SET quantidade = ".($row["quantidade"]+1)." WHERE cpf_cnpj_cli = '".$_SESSION['nome']."' AND codigo_prod = '$codigo'";
+} else {
+    $sql = "INSERT INTO `carrinho` (`cpf_cnpj_cli`, `codigo_prod`, `quantidade`) VALUES ('".$_SESSION['nome']."', '$codigo', 1)";
+}
+
+echo $sql;
+
+$bd->beginTransaction();
+$linhas = $bd->exec($sql);
+if ($linhas == 1){
+    $bd->commit();
+} else {
+    $bd->rollBack();
+};
